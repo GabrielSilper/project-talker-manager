74db638fd233be78d7d96cb5ddcc7d8bf7e47767
GabrielSilper <gabrielsilper.main@gmail.com>
2023-04-14 19:03:48 -0400

    R06 - Criação do endpoint PUT /talker/:id, valida e depois atualiza

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const { readTalkerJson, writeTalkerJson } = require('../utils/crudFileFunctions');
-const validateAuthorization = require('../middlewares/validateAuthorization');
+const express = require("express");
+const {
+  readTalkerJson,
+  writeTalkerJson,
+} = require("../utils/crudFileFunctions");
+const validateAuthorization = require("../middlewares/validateAuthorization");
 const {
   validateTalkerFields,
   validateTalkerContents,
   validatePropertyTalkFields,
   validatePropertyTalkContents,
-} = require('../middlewares/validateTalker');
+} = require("../middlewares/validateTalker");
 
 const talkerRoutes = express.Router();
 
-talkerRoutes.get('/', async (req, res) => {
+talkerRoutes.get("/", async (req, res) => {
   const talkers = await readTalkerJson();
   return res.status(200).json(talkers);
 });
 
-talkerRoutes.get('/:id', async (req, res, next) => {
+talkerRoutes.get("/:id", async (req, res, next) => {
   const { id } = req.params;
   const talkers = await readTalkerJson();
   if (Number(id) >= 0) {
     const foundTalker = talkers.find((talker) => talker.id === Number(id));
     if (!foundTalker) {
       return res.status(404).json({
-        message: 'Pessoa palestrante não encontrada',
+        message: "Pessoa palestrante não encontrada",
       });
     }
     return res.status(200).json(foundTalker);
@@ -38,12 +41,33 @@ talkerRoutes.use(validatePropertyTalkFields);
 talkerRoutes.use(validatePropertyTalkContents);
 talkerRoutes.use(validateTalkerContents);
 
-talkerRoutes.post('/', async (req, res) => {
+talkerRoutes.post("/", async (req, res) => {
   const talker = req.body;
   const talkers = await readTalkerJson();
   const newTalkers = [...talkers, { id: talkers.length + 1, ...talker }];
   await writeTalkerJson(newTalkers);
   res.status(201).json({ id: talkers.length + 1, ...talker });
+});
+
+talkerRoutes.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const talkerEdit = req.body;
+  const talkers = await readTalkerJson();
+  const talkerPosition = talkers.findIndex(
+    (talker) => talker.id === Number(id)
+  );
+
+  if (talkerPosition < 0) {
+    return res.status(404).json({
+      message: "Pessoa palestrante não encontrada",
+    });
+  }
+
+  talkers[talkerPosition] = { id: Number(id), ...talkerEdit };
+
+  await writeTalkerJson(talkers);
+
+  res.status(200).json({ id: Number(id), ...talkerEdit });
 });
 
 module.exports = talkerRoutes;
