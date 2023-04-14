4faba6ac000f9243c43fe47e262fd82b5f9940b1
GabrielSilper <gabrielsilper.main@gmail.com>
2023-04-14 14:49:39 -0400

    R02 - Endpoint GET /talker/:id feito

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -1,11 +1,28 @@
-const express = require('express');
-const { readTalkerJson } = require('../utils/crudFileFunctions');
+const express = require("express");
+const { readTalkerJson } = require("../utils/crudFileFunctions");
 
 const talkerRoutes = express.Router();
 
-talkerRoutes.get('/', async (req, res) => {
+talkerRoutes.get("/", async (req, res) => {
   const talkers = await readTalkerJson();
-  res.status(200).json(talkers);
+  return res.status(200).json(talkers);
+});
+
+talkerRoutes.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const talkers = await readTalkerJson();
+  if (Number(id) >= 0) {
+    const foundTalker = talkers.find((talker) => talker.id === Number(id));
+    if (!foundTalker) {
+      return res.status(404).json({
+        message: "Pessoa palestrante não encontrada",
+      });
+    } else {
+      return res.status(200).json(foundTalker);
+    }
+  }else {
+    next();
+  }
 });
 
 module.exports = talkerRoutes;
